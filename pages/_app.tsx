5c84c5a4191ee0c90a25d7e56d83a3d6ce45482d
Harsh <harshgajjar145@gmail.com>
2022-12-30 12:45:47 +0530

    fea: livepeer upload assets done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/theme'
+import { createReactClient, LivepeerConfig, studioProvider } from '@livepeer/react';
+
+const client = createReactClient({
+  provider: studioProvider({ apiKey: 'be276656-3e1a-4b0b-b42b-61e8461b1fd4' }),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <LivepeerConfig client={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </LivepeerConfig>
   )
 }
