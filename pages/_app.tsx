be0a46dbbb7648a807dfac8e21141ab2a33c6117
Harsh <harshgajjar145@gmail.com>
2022-12-30 12:47:52 +0530

    api removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import theme from '../styles/theme'
 import { createReactClient, LivepeerConfig, studioProvider } from '@livepeer/react';
 
 const client = createReactClient({
-  provider: studioProvider({ apiKey: 'be276656-3e1a-4b0b-b42b-61e8461b1fd4' }),
+  provider: studioProvider({ apiKey: 'enter your api' }),
 });
 
 export default function App({ Component, pageProps }: AppProps) {
