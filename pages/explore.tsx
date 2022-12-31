9b4508c3dca793249bb74d8e922325e12895c36c
Harsh <harshgajjar145@gmail.com>
2022-12-31 13:19:09 +0530

    fix: refactor

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Button,
+  Center,
   chakra,
   Container,
   Flex,
@@ -12,134 +13,14 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import orbis from "./orbis";
-import { useEffect, useState } from "react";
-
-const testimonials = [
-  {
-    name: "Brandon P.",
-    role: "Chief Marketing Officer",
-    content:
-      "Thank you for making it painless, pleasant and most of all, hassle free! Im good to go.No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
-    avatar:
-      "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  },
-  {
-    name: "Krysta B.",
-    role: "Entrepreneur",
-    content:
-      "I didn't even need training. We've used EEZY for the last five years. I have gotten at least 50 times the value from EEZY. I made back the purchase price in just 48 hours!",
-    avatar:
-      "https://images.unsplash.com/photo-1598550874175-4d0ef436c909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-  },
-  {
-    name: "Darcy L.",
-    role: "Movie star",
-    content:
-      "Thank you for making it painless, pleasant and most of all, hassle free! I'm good to go. No matter where you go, EEZY is the coolest, most happening thing around! I love EEZY!",
-    avatar:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80",
-  },
-];
-
-const backgrounds = [
-  `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ED8936'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%2348BB78'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%230BC5EA'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
-];
-
-interface TestimonialCardProps {
-  name: string;
-  role: string;
-  content: string;
-  avatar: string;
-  index: number;
-}
-
-
-function TestimonialCard(props: TestimonialCardProps) {
-  console.log(props);
-  const {  content, index } = props;
-  
-  return (
-    <Flex
-      boxShadow={"lg"}
-      maxW={"640px"}
-      direction={{ base: "column-reverse", md: "row" }}
-      width={"full"}
-      rounded={"xl"}
-      justifyContent={"space-between"}
-      position={"relative"}
-      bg={useColorModeValue("white", "gray.800")}
-      _before={{
-        content: '""',
-        position: "absolute",
-        zIndex: "-1",
-        height: "full",
-        maxW: "640px",
-        width: "full",
-        filter: "blur(40px)",
-        transform: "scale(0.98)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        top: 0,
-        left: 0,
-        backgroundImage: backgrounds[index % 4],
-      }}
-    >
-      <Flex
-        direction={"column"}
-        textAlign={"left"}
-        justifyContent={"space-between"}
-      >
-        <Image
-          src={'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'}
-          alt=""
-          h="200px"
-          roundedTop={"lg"}
-          objectFit="contain"
-          bg="white"
-        />
-        <Box p="5">
-          <chakra.p fontWeight={"medium"} fontSize={"15px"} pb={4}>
-            {content.title}
-          </chakra.p>
-          <chakra.p fontWeight={"medium"} fontSize={"15px"} pb={4}>
-            {content.body}
-          </chakra.p>
-          <Button
-            w="full"
-            onClick={() => {
-              var unlockProtocolConfig = {
-                locks: {
-                  "0xc37ffe60f6c3830ed0e92939d41ad1ecf8fd46d9": {
-                    network: 5,
-                    skipRecipient: true,
-                  },
-                },
-                pessimistic: true,
-                skipRecipient: true,
-              };
-              //Load above config in a window
-              window.location.replace(
-                "https://app.unlock-protocol.com/checkout?paywallConfig=%7B%22locks%22%3A%7B%220xc37ffe60f6c3830ed0e92939d41ad1ecf8fd46d9%22%3A%7B%22network%22%3A5%2C%22skipRecipient%22%3Atrue%7D%7D%2C%22pessimistic%22%3Atrue%2C%22skipRecipient%22%3Atrue%7D"
-              );
-            }}
-          >
-            Create
-          </Button>
-        </Box>
-      </Flex>
-    </Flex>
-  );
-}
+import { useState } from "react";
+import VideoCard from "../components/VideoCard";
 
 export default function GridBlurredBackdrop() {
   const [response, setResponse] = useState();
   async function connect() {
     let res = await orbis.connect();
 
-    /** Check if connection is successful or not */
     if (res.status == 200) {
       // setUser(res.did);
     } else {
@@ -151,18 +32,13 @@ export default function GridBlurredBackdrop() {
   async function getPost() {
     // console.log(res.doc);
     await connect();
-    let { data, error } = await orbis.getPosts(
-      { tag: "Test Tag" }
-    );
+    let { data, error } = await orbis.getPosts({ tag: "Test Tag" });
     console.log(data);
     setResponse(data);
     console.log(response);
   }
 
   return (
-
-
-
     <Flex
       textAlign={"center"}
       p={[2, 10]}
@@ -177,28 +53,28 @@ export default function GridBlurredBackdrop() {
           fontWeight={"bold"}
           color={useColorModeValue("gray.700", "gray.50")}
         >
-          Upload your assets
+          Videos
         </Heading>
       </Box>
 
-
-
-
-      {response ? (<SimpleGrid
-        columns={{ base: 1, md: 2, xl: 3 }}
-        spacing={"20"}
-        maxW={"container.xl"}
-        my={16}
-        mx={"auto"}
-      >
-        {response &&  response.map((cardInfo:any, index:number) => (
-          <TestimonialCard {...cardInfo} index={index} key={index} />
-        ))}
-      </SimpleGrid>) : (<Button onClick={getPost}>
-        Show Videos
-
-      </Button>)}
-
+      {response ? (
+        <SimpleGrid
+          columns={{ base: 1, md: 2, xl: 3 }}
+          spacing={"20"}
+          maxW={"container.xl"}
+          my={16}
+          mx={"auto"}
+        >
+          {response &&
+            response.map((cardInfo: any, index: number) => (
+              <VideoCard {...cardInfo} index={index} key={index} />
+            ))}
+        </SimpleGrid>
+      ) : (
+        <Center h='50vh'>
+          <Button onClick={getPost}>Show Videos</Button>
+        </Center>
+      )}
     </Flex>
   );
 }
--- pages/video.tsx
+++ pages/video.tsx
@@ -23,7 +23,7 @@ import {
   HStack,
 } from "@chakra-ui/react";
 import { useCreateAsset } from "@livepeer/react";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import { ethers } from "ethers";
 import { UnlockV11, PublicLockV11 } from "@unlock-protocol/contracts";
@@ -39,20 +39,20 @@ import {
   usePrepareContractWrite,
 } from "wagmi";
 
-const MotionBox = motion(Box);
 const lockInterface = new ethers.utils.Interface(PublicLockV11.abi);
+
 export default function UploadVideo() {
   const { address: creator } = useAccount();
   const [calldata, setCalldata] = useState("");
-  const [playbackID, setplaybackID] = useState<string|undefined>("");
+  const [playbackID, setplaybackID] = useState<string | undefined>("");
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [duration, setDuration] = useState(99999);
   const [supply, setSupply] = useState(99999);
   const [price, setPrice] = useState(0);
   const [currency, setCurrency] = useState("");
-  const [enableDuration, setEnableDuration] = useState(true);
-  const [enableSupply, setEnableSupply] = useState(true);
+  const [enableDuration, setEnableDuration] = useState(false);
+  const [enableSupply, setEnableSupply] = useState(false);
   const [video, setVideo] = useState<File | undefined>(undefined);
   const {
     mutate: createAsset,
@@ -65,8 +65,8 @@ export default function UploadVideo() {
     // for the returned data
     video
       ? {
-          sources: [{ name: video.name, file: video }] as const,
-        }
+        sources: [{ name: video.name, file: video }] as const,
+      }
       : null
   );
 
@@ -100,6 +100,7 @@ export default function UploadVideo() {
   } = useWaitForTransaction({
     hash: transaction?.hash,
   });
+
   async function connect() {
     let res = await orbis.connect();
 
@@ -111,6 +112,7 @@ export default function UploadVideo() {
       alert("Error connecting to Ceramic.");
     }
   }
+
   const prepareCalldata = async () => {
     setCalldata(
       lockInterface.encodeFunctionData(
@@ -130,270 +132,240 @@ export default function UploadVideo() {
   async function getPost() {
     // console.log(res.doc);
     let { data, error } = await orbis.getPosts(
-     { tag:"Test Tag"}
+      { tag: "Test Tag" }
     );
     console.log(data);
   }
-  async function createPost (name:string, playbackId:string|undefined,description:string) {
+  async function createPost(name: string, playbackId: string | undefined, description: string) {
     await connect();
-    let res = await orbis.createPost({ body: description,
-  title:name,
-  data:{
-    // unlockAddress:"0xc37ffe60f6c3830ed0e92939d41ad1ecf8fd46d9",
-    // creatorName:"Rahul"
-     playbackID:playbackId
-  },
-  
-  tags:[
-   { slug:"Test Tag",title:"Courses"}
-  ]
-  });
-    console.log("Created post:",res.doc);
+    let res = await orbis.createPost({
+      body: description,
+      title: name,
+      data: {
+        // unlockAddress:"0xc37ffe60f6c3830ed0e92939d41ad1ecf8fd46d9",
+        // creatorName:"Rahul"
+        playbackID: playbackId
+      },
+
+      tags: [
+        { slug: "Test Tag", title: "Courses" }
+      ]
+    });
+    console.log("Created post:", res.doc);
     await getPost();
   }
 
-  console.log("Assets is",assets);
+  console.log("Assets is", assets);
   useEffect(() => {
-   
+
     if (status === "success") {
-            //  setplaybackID(assets[0].playbackId ?? "");
-            
-        console.log("PlaybackID is",assets[0].playbackId);
-        console.log(playbackID);
-       createPost(name,assets[0].playbackId,description);
-            toast({
-          title: 'Successfully created your service, please sign message to create post',
-          // description: "We've created your account for you.",
-          status: 'success',
-          duration: 5000,
-          isClosable: true,
-        })
-     
+      //  setplaybackID(assets[0].playbackId ?? "");
+
+      console.log("PlaybackID is", assets[0].playbackId);
+      console.log(playbackID);
+      createPost(name, assets[0].playbackId, description);
+      toast({
+        title: 'Successfully created your service, please sign message to create post',
+        // description: "We've created your account for you.",
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
+
     }
-    
+
   }, [status]);
   console.log(progress);
+
+  const progressFormatted = useMemo(
+    () =>
+      progress?.[0].phase === 'failed'
+        ? 'Failed to process video.'
+        : progress?.[0].phase === 'waiting'
+          ? 'Waiting'
+          : progress?.[0].phase === 'uploading'
+            ? <Progress value={Math.round(progress?.[0]?.progress * 100)} />
+            : progress?.[0].phase === 'processing'
+              ? <Progress value={Math.round(progress?.[0]?.progress * 100)} />
+              : null,
+    [progress],
+  );
+
   return (
     <Box position={"relative"}>
       <Navbar />
-      <Container
-        as={SimpleGrid}
-        maxW={"7xl"}
-        columns={{ base: 1, md: 2 }}
-        spacing={{ base: 10, lg: 32 }}
-        py={{ base: 10, sm: 20, lg: 32 }}
-      >
-        <Stack spacing={{ base: 10, md: 20 }} zIndex={2}>
-          <Heading
-            lineHeight={1.1}
-            fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
-          >
-            Senior web designers{" "}
-            <Text
-              as={"span"}
-              bgGradient="linear(to-r, red.400,pink.400)"
-              bgClip="text"
-            >
-              &
-            </Text>{" "}
-            Full-Stack Developers
-          </Heading>
-        </Stack>
-        <Stack
-          bg={"gray.200"}
-          rounded={"xl"}
-          p={{ base: 4, sm: 6, md: 8 }}
-          spacing={{ base: 8 }}
-          maxW={{ lg: "lg" }}
-          zIndex={2}
+      <Center>
+        <Container
+          maxW={"lg"}
+          py={[4, 8]}
         >
-          <Stack spacing={4}>
-            <Heading
-              color={"gray.800"}
-              lineHeight={1.1}
-              fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
-            >
-              Upload a video
-              <Text
-                as={"span"}
-                bgGradient="linear(to-r, red.400,pink.400)"
-                bgClip="text"
-              >
-                !
-              </Text>
-            </Heading>
-            <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
-              We’re looking for amazing engineers just like you! Become a part
-              of our rockstar engineering team and skyrocket your career!
-            </Text>
-          </Stack>
-          <Box as={"form"} mt={10}>
+          <Stack
+            bg={"gray.200"}
+            rounded={"xl"}
+            p={{ base: 4, sm: 6, md: 8 }}
+            spacing={{ base: 8 }}
+            maxW={{ lg: "lg" }}
+          >
             <Stack spacing={4}>
-              <Input
-                placeholder="Name"
-                bg={"gray.100"}
-                border={0}
-                color={"gray.500"}
-                _placeholder={{
-                  color: "gray.500",
-                }}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-               <Textarea
-                placeholder="Discription"
-                bg={"gray.100"}
-                border={0}
-                color={"gray.500"}
-                _placeholder={{
-                  color: "gray.500",
-                }}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-              {/* <FormControl display="flex" alignItems="center" gap={8}>
-                <HStack>
-                  <FormLabel mb="0" color={"black"}>
-                    Enable duration?
-                  </FormLabel>
-                  <Switch size={"md"} />
-                </HStack>
-                <HStack>
-                  <FormLabel mb="0" color={"black"}>
-                    Enable supply?
-                  </FormLabel>
-                  <Switch size={"md"} />
-                </HStack>
-              </FormControl> */}
-              <Flex gap={8}>
-                <Input
-                  placeholder="duration"
-                  bg={"gray.100"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                  type="number"
-                  onChange={(e) => {
-                    setDuration(parseInt(e.target.value));
-                  }}
-                />
-                <Input
-                  placeholder="supply"
-                  bg={"gray.100"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{
-                    color: "gray.500",
-                  }}
-                  type="number"
-                  onChange={(e) => {
-                    setSupply(parseInt(e.target.value));
-                  }}
-                />
-              </Flex>
-              <Input
-                placeholder="Price"
-                bg={"gray.100"}
-                border={0}
-                color={"gray.500"}
-                _placeholder={{
-                  color: "gray.500",
-                }}
-                type="number"
-                onChange={(e) => {
-                  setPrice(parseInt(e.target.value));
-                }}
-              />
-
-              <Center w="full">
-                <label
-                  htmlFor="video"
-                  style={{
-                    backgroundColor: "black",
-                    textAlign: "center",
-                    padding: "10px",
-                    fontWeight: "500",
-                    borderRadius: "4px",
-                    color: "white",
-                    cursor: "pointer",
-                    width: "100%",
-                  }}
+              <Heading
+                color={"gray.800"}
+                lineHeight={1.1}
+                fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+              >
+                Upload a video
+                <Text
+                  as={"span"}
+                  bgGradient="linear(to-r, red.400,pink.400)"
+                  bgClip="text"
                 >
-                  Select a video
-                </label>
-                <input
-                  type="file"
-                  id="video"
-                  onChange={(e) => {
-                    if (e.target.files) {
-                      setVideo(e.target.files[0]);
-                    }
-                  }}
-                  style={{ display: "none" }}
-                />
-              </Center>
+                  !
+                </Text>
+              </Heading>
+              <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
+                We’re looking for amazing engineers just like you! Become a part
+                of our rockstar engineering team and skyrocket your career!
+              </Text>
             </Stack>
-            {process ? (
-              <Progress value={progress * 100} />
-            ) : <></>}
-            <Button
-              fontFamily={"heading"}
-              mt={8}
-              w={"full"}
-              bgGradient="linear(to-r, red.400,pink.400)"
-              color={"white"}
-              onClick={() => {
-                createService();
-              }}
-              _hover={{
-                bgGradient: "linear(to-r, red.400,pink.400)",
-                boxShadow: "xl",
-              }}
-              disabled={isLoading}
-            >
-              Submit
-            </Button>
-          </Box>
-          form
-        </Stack>
-      </Container>
-      <MotionBox
-        animate={{ rotate: 360 }}
-        transition={{ repeat: Infinity, duration: 10 }}
-        maxW={["300px", "xs"]}
-        position={"absolute"}
-        top={-20}
-        left={-20}
-        style={{ filter: "blur(70px)" }}
-      >
-        <Blur />
-      </MotionBox>
+            <Box as={"form"} mt={10}>
+              <Stack spacing={4}>
+                <Input
+                  placeholder="Name"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                <Textarea
+                  placeholder="Discription"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                />
+                <Input
+                  placeholder="Price"
+                  bg={"gray.100"}
+                  border={0}
+                  color={"gray.500"}
+                  _placeholder={{
+                    color: "gray.500",
+                  }}
+                  type="number"
+                  onChange={(e) => {
+                    setPrice(parseInt(e.target.value));
+                  }}
+                />
+                <Box bg='gray.800' p={4} rounded='lg'>
+                  <FormControl display="flex" alignItems="center" gap={8}>
+                    <HStack alignItems={'center'}>
+                      <FormLabel mb={0}>
+                        Enable duration?
+                      </FormLabel>
+                      <Switch size={"md"} colorScheme='red' onChange={() => setEnableDuration(!enableDuration)} />
+                    </HStack>
+                    <HStack alignItems={'center'}>
+                      <FormLabel mb={0}>
+                        Enable supply?
+                      </FormLabel>
+                      <Switch size={"md"} colorScheme='red' onChange={() => setEnableSupply(!enableSupply)} />
+                    </HStack>
+                  </FormControl>
+                  <Flex gap={8} mt={4}>
+                    {
+                      enableDuration ? (
+                        <Input
+                          placeholder="duration"
+                          bg={"gray.100"}
+                          border={0}
+                          color={"gray.500"}
+                          _placeholder={{
+                            color: "gray.500",
+                          }}
+                          type="number"
+                          onChange={(e) => {
+                            setDuration(parseInt(e.target.value));
+                          }}
+                        />
+                      ) : null
+                    }
+                    {enableSupply ? (
+                      <Input
+                        placeholder="supply"
+                        bg={"gray.100"}
+                        border={0}
+                        color={"gray.500"}
+                        _placeholder={{
+                          color: "gray.500",
+                        }}
+                        type="number"
+                        onChange={(e) => {
+                          setSupply(parseInt(e.target.value));
+                        }}
+                      />
+                    ) : null}
+                  </Flex>
+                </Box>
+                <Center w="full">
+                  <label
+                    htmlFor="video"
+                    style={{
+                      backgroundColor: "black",
+                      textAlign: "center",
+                      padding: "10px",
+                      fontWeight: "500",
+                      borderRadius: "4px",
+                      color: "white",
+                      cursor: "pointer",
+                      width: "100%",
+                    }}
+                  >
+                    Select a video
+                  </label>
+                  <input
+                    type="file"
+                    id="video"
+                    onChange={(e) => {
+                      if (e.target.files) {
+                        setVideo(e.target.files[0]);
+                      }
+                    }}
+                    style={{ display: "none" }}
+                  />
+                </Center>
+              </Stack>
+              {progressFormatted && <Text>{progressFormatted}</Text>}
+              <Button
+                fontFamily={"heading"}
+                mt={8}
+                w={"full"}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                color={"white"}
+                onClick={() => {
+                  createService();
+                }}
+                _hover={{
+                  bgGradient: "linear(to-r, red.400,pink.400)",
+                  boxShadow: "xl",
+                }}
+                disabled={isLoading}
+              >
+                Submit
+              </Button>
+            </Box>
+            form
+          </Stack>
+        </Container>
+      </Center>
     </Box>
   );
 }
-
-export const Blur = (props: IconProps) => {
-  return (
-    <Icon
-      width={useBreakpointValue({ base: "100%", md: "40vw", lg: "30vw" })}
-      zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-      height="560px"
-      viewBox="0 0 528 560"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <circle cx="71" cy="61" r="111" fill="#F56565" />
-      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-      <circle cy="291" r="139" fill="#ED64A6" />
-      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
-    </Icon>
-  );
-};
